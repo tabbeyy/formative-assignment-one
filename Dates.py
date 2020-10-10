35ef2ac91acecfd27b31c9a0c04de90bcf36c530
Arijus <aryjeleng@gmail.com>
2020-10-10 19:07:34 +0100

    Please save me

--- Dates.py
+++ Dates.py
@@ -18,7 +18,7 @@ month = 10
 day = 9
 k = 200069
 day += k
-feb = leap_check(year)#df
+feb = leap_check(year)#dfcc
 
 while day > 28:
     if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
